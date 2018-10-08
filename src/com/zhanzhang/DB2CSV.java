dda378ef6234887fb99e2e020c6013ee41c2332d
Zhan Zhang <klosezhang@yahoo.com.au>
2018-10-08 11:52:53 +1100

    Performance improvement

--- src/com/zhanzhang/DB2CSV.java
+++ src/com/zhanzhang/DB2CSV.java
@@ -1,9 +1,12 @@
 package com.zhanzhang;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
@@ -11,6 +14,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Calendar;
 import java.util.Properties;
 
 /**
@@ -18,10 +23,19 @@ import java.util.Properties;
  */
 public class DB2CSV {
 
-    /** Dump the whole database to an SQL string */
+    /** Dump the whole database to CSV files */
     public static String dumpDB(Properties props) {
         String driverClassName = props.getProperty("jdbc.driver.class");
         String driverURL = props.getProperty("jdbc.driver.url");
+        String buffer = props.getProperty("buffer");
+        int bufferSize = 1048576;// Default buffer size is 1024 * 1024 bytes (1MB)
+        if (buffer!=null && buffer.trim().length()>0) {
+        	try {
+        		bufferSize = Integer.parseInt(buffer);
+        	} catch (Exception e) {
+        		
+        	}
+        }
         DatabaseMetaData dbMetaData = null;
         Connection dbConn = null;
         try {
@@ -30,7 +44,7 @@ public class DB2CSV {
             dbMetaData = dbConn.getMetaData();
         }
         catch( Exception e ) {
-            System.err.println("Unable to connect to database: "+e);
+            System.err.println("Unable to connect to database: " + e);
             return null;
         }
 
@@ -39,9 +53,16 @@ public class DB2CSV {
             String catalog = props.getProperty("catalog");
             String schema = props.getProperty("schemaPattern");
             String tables = props.getProperty("tableName");
+            String exportLocation = props.getProperty("export.location");
+            if (tables!=null && tables.trim().length()==0)
+            	tables = null;
+            if (exportLocation==null || exportLocation.trim().length()==0) {
+            	System.err.println("Failure: export.location is not set");
+                return null;
+            }
             ResultSet rs = dbMetaData.getTables(catalog, schema, tables, null);
             if (! rs.next()) {
-                System.err.println("Unable to find any tables matching: catalog="+catalog+" schema="+schema+" tables="+tables);
+                System.err.println("Unable to find any tables matching: catalog=" + catalog + " schema=" + schema + " tables=" + tables);
                 rs.close();
             } else {
                 do {
@@ -50,6 +71,10 @@ public class DB2CSV {
                     System.out.println("Working on table: " + tableName);
                     String tableType = rs.getString("TABLE_TYPE");
                     if ("TABLE".equalsIgnoreCase(tableType)) {
+                    	File file = new File(exportLocation + "/" + tableName + ".csv");
+                    	if (file.exists()) {
+                    		file.delete();
+                    	}
                         ResultSet tableMetaData = dbMetaData.getColumns(null, null, tableName, "%");
                         boolean firstLine = true;
                         while (tableMetaData.next()) {
@@ -66,18 +91,9 @@ public class DB2CSV {
                         }
                         tableMetaData.close();
                         // Populate the values
-                        dumpTable(dbConn, result, tableName);
-                        File directory = new File (props.getProperty("export.location"));
-                        if (!directory.exists()) {
-                        	directory.mkdirs();
-                        }
-                        File file = new File (directory.getAbsolutePath() + "/" +tableName+".csv");
-                        if (!file.exists())
-                        	file.createNewFile();
-                        FileWriter fr = new FileWriter(file);
-                        fr.write(result.toString());
-                        fr.close();
-                        System.out.println("Table: " + tableName + " has been exported into: " + file.getAbsolutePath());
+                        dumpTable(dbConn, result, tableName, bufferSize, exportLocation);
+                        writeToFile(tableName, result.toString(), exportLocation);
+                        System.out.println("Table: " + tableName + " has been exported into: " + exportLocation);
                         result = new StringBuffer();
                     }
                 } while (rs.next());
@@ -91,11 +107,12 @@ public class DB2CSV {
         return null;
     }
 
-    /** dump this particular table data */
-    private static void dumpTable(Connection dbConn, StringBuffer result, String tableName) {
+    /** dump this particular table data 
+     * @throws IOException */
+    private static void dumpTable(Connection dbConn, StringBuffer result, String tableName, int bufferSize, String exportLocation) throws IOException {
         try {
             // First we output the create table stuff
-            PreparedStatement stmt = dbConn.prepareStatement("SELECT * FROM "+tableName);
+            PreparedStatement stmt = dbConn.prepareStatement("SELECT * FROM " + tableName);
             ResultSet rs = stmt.executeQuery();
             ResultSetMetaData metaData = rs.getMetaData();
             int columnCount = metaData.getColumnCount();
@@ -109,17 +126,49 @@ public class DB2CSV {
                     if (value == null) {
                         result.append("");
                     } else {
-                        String outputValue = value.toString();
-                        result.append(outputValue);
+                    	int type = metaData.getColumnType(i+1);
+                        if (type == Types.BLOB) {	//Process Blob text (Not LONGVARBINARY)
+        					InputStream inputStream = ((Blob)value).getBinaryStream();
+        					InputStreamReader isr=new InputStreamReader(inputStream);
+        					BufferedReader br=new BufferedReader(isr);
+        					String line;
+        					while ((line=br.readLine())!=null) {
+        						result.append("\"");	//Put double quote before and after string to escape comma
+        						result.append(line);
+        						result.append("\"");
+        						result.append("\n");
+        					}
+        					inputStream.close();
+                    	} else {
+                    		result.append(value.toString());
+                    	}
                     }
                 }
                 result.append("\n");
+                if (result.length() >= bufferSize) {
+                	writeToFile(tableName, result.toString(), exportLocation);
+                	System.out.println(Calendar.getInstance().getTime() + " Exporting table: " + tableName + "'s " + result.length() / 1024 + "KB data into " + exportLocation + " to clean up memory buffer");
+                	result.delete(0, result.length());
+                }
             }
             rs.close();
             stmt.close();
         } catch (SQLException e) {
-            System.err.println("Unable to dump table "+tableName+" because: "+e);
+            System.err.println("Unable to dump table " + tableName + " because: " + e);
         }
+    }
+    
+    private static void writeToFile(String tableName, String content, String exportLocation) throws IOException {
+    	File directory = new File (exportLocation);
+        if (!directory.exists()) {
+        	directory.mkdirs();
+        }
+        File file = new File (directory.getAbsolutePath() + "/" +tableName + ".csv");
+        if (!file.exists())
+        	file.createNewFile();
+        FileWriter fr = new FileWriter(file, true);	//Append the content to the end
+        fr.write(content);
+        fr.close();
     }
 
     public static void main(String[] args) {
@@ -131,7 +180,7 @@ public class DB2CSV {
             dumpDB(props);
             System.out.println("Done");
         } catch (IOException e) {
-            System.err.println("Unable to open property file: configuration.properties exception: "+e);
+            System.err.println("Unable to open property file: configuration.properties exception: " + e);
         }
 
     }
